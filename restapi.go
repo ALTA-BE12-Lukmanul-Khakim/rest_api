3a0335518fb5b14bddaeef29bba1feca0862a142
lukmanul <klukmanul33@gmail.com>
2022-10-05 08:01:22 +0700

    check program -login email only

--- restapi.go
+++ restapi.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"net/http"
-	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -22,10 +21,10 @@ type User struct {
 type Vendor struct {
 	Name_co   string `json:"name_co" form:"name_co"`
 	Expedisi  string `json:"expedisi" form:"expedisi"`
-	Transpot  string `json:"tansport" form:"transport"`
-	Time_go   time.Time
-	Time_come time.Time
-	Is_done   bool
+	Transport string `json:"transport" form:"transport"`
+	// Time_go   time.Time
+	// Time_come time.Time
+	Is_done bool
 }
 
 func connectDBGorm() *gorm.DB {
@@ -38,22 +37,22 @@ func Regist(db *gorm.DB) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		var user User
 		if err := c.Bind(&user); err != nil {
+			log.Error(err)
 			return c.JSON(http.StatusBadRequest, map[string]interface{}{
 				"message": "cannot read data",
 			})
 		}
+		Pass, _ := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost) //dycrypt password
+		user.Password = string(Pass)
 		newuser := User{
 			Name:     user.Name,
 			Email:    user.Email,
 			Password: user.Password,
 			Hp:       user.Hp,
 		}
-		Pass, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost) //dycrypt password
-		if err != nil {
-			return err
-		}
-		newuser.Password = string(Pass)
-		if err := db.Create(&newuser); err != nil {
+
+		if err := db.Create(&newuser).Error; err != nil {
+			log.Error(err)
 			return c.JSON(http.StatusInternalServerError, map[string]interface{}{
 				"message": "cannot insert data",
 			})
@@ -69,20 +68,20 @@ func Regist(db *gorm.DB) echo.HandlerFunc {
 func Login(db *gorm.DB) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		email := c.Param("email")
-		password := c.Param("password")
+		//	password := c.Param("password")
 		var resQry User
 
-		//check password
-		err := bcrypt.CompareHashAndPassword([]byte(resQry.Password), []byte(password))
-		if err != nil {
-			return err
-		}
 		//check email
 		if err := db.First(&resQry, "email = ?", email).Error; err != nil {
 			return c.JSON(http.StatusInternalServerError, map[string]interface{}{
-				"message": "wrong email",
+				"message": "wrong email ",
 			})
 		}
+		//check password
+		// err := bcrypt.CompareHashAndPassword([]byte(resQry.Password), []byte(password))
+		// if err != nil {
+		// 	return err
+		// }
 
 		return c.JSON(http.StatusOK, map[string]interface{}{
 			"message": "success get specific data",
@@ -110,10 +109,10 @@ func GetAllvendor(db *gorm.DB) echo.HandlerFunc {
 
 func GetDataVendor(db *gorm.DB) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		exp := c.Param("expedisi")
+		expedisi := c.Param("expedisi")
 
-		var ven Vendor
-		if err := db.First(&ven, "expedisi = ?", exp).Error; err != nil {
+		var ven []Vendor
+		if err := db.First(&ven, "expedisi = ?", expedisi).Error; err != nil {
 			return c.JSON(http.StatusInternalServerError, map[string]interface{}{
 				"message": "cannot select data",
 			})
@@ -129,7 +128,7 @@ func AddVendor(db *gorm.DB) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		var newven Vendor
 
-		if err := c.Bind(newven).Error; err != nil {
+		if err := c.Bind(&newven); err != nil {
 			log.Error(err)
 			return c.JSON(http.StatusBadRequest, map[string]interface{}{
 				"message": "cannot read data",
@@ -162,7 +161,7 @@ func main() {
 	e.Use(middleware.Logger())
 
 	e.POST("/users", Regist(db))
-	e.GET("/users", Login(db))
+	e.GET("/users/:email", Login(db))
 	e.GET("/vendors", GetAllvendor(db))
 	e.GET("/vendors/:expedisi", GetDataVendor(db))
 	e.POST("/vendors", AddVendor(db))
